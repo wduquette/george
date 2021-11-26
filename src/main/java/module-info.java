84a30aed7dc41dbedcbc90f3de029e4a003faf39
Will Duquette <will@wjduquette.com>
2021-11-26 14:38:30 -0800

    Can load tmx json file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.wjduquette.george {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires com.google.gson;
 
     opens com.wjduquette.george to javafx.fxml;
     exports com.wjduquette.george;
+
+    opens com.wjduquette.george.tmx to com.google.gson;
+    exports com.wjduquette.george.tmx;
 }
