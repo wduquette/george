789e2a77f2e3f1e8b33a498c617474dec3c2b07a
Will Duquette <will@wjduquette.com>
2021-11-28 14:34:20 -0800

    App positions George at the "origin".

--- src/main/java/com/wjduquette/george/App.java
+++ src/main/java/com/wjduquette/george/App.java
@@ -1,9 +1,6 @@
 package com.wjduquette.george;
 
-import com.wjduquette.george.graphics.TerrainTileSet;
-import com.wjduquette.george.graphics.TileSet;
 import com.wjduquette.george.model.RegionMap;
-import com.wjduquette.george.model.TerrainType;
 import com.wjduquette.george.widgets.CanvasPane;
 import com.wjduquette.george.ecs.*;
 import com.wjduquette.george.widgets.MapViewer;
@@ -11,50 +8,30 @@ import javafx.application.Application;
 import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
     private final CanvasPane canvas = new CanvasPane();
-    private TileSet mobiles;
-    private RegionMap overworld;
 
     @Override
     public void start(Stage stage) {
-        mobiles = new TileSet(getClass(), "assets/tilesets/mobiles.tileset");
-
-
-//        // first, initialize the world.
-//        for (int r = -5; r <= 5; r++) {
-//            for (int c = -5; c <= 5; c++) {
-//                String tile = Math.abs(r) > 2 || Math.abs(c) > 2
-//                    ? "standard.grass" : "standard.tile_floor";
-//                Image img = terrain.get(tile).orElseThrow().image();
-//                world.make().putTerrain(TerrainType.FLOOR)
-//                    .putCell(r,c)
-//                    .putTile(img);
-//            }
-//        }
-//
-//        world.make().putMobile()
-//            .putCell(0,0)
-//            .putTile(mobiles.get("mobile.george").orElseThrow());
-
-        overworld = new RegionMap(getClass(),
+        RegionMap overworld = new RegionMap(getClass(),
             "assets/regions/overworld/overworld.region");
 
+        Cell origin = overworld.query(Point.class)
+            .filter(e -> e.point().name().equals("origin"))
+            .map(Entity::cell)
+            .findFirst()
+            .orElse(new Cell(10,10));
+
         overworld.getEntities().make().putMobile()
-            .putCell(10,10)
-            .putTile(mobiles.get("mobile.george").orElseThrow());
+            .putCell(origin)
+            .putTile(TileSets.MOBILES.get("mobile.george").orElseThrow());
 
         MapViewer viewer = new MapViewer();
         viewer.setMap(overworld);
 
         // NEXT, configure the GUI
-        // TODO: revise to display the region.
-//        canvas.setOnResize(() -> renderRegion(overworld));
-
-//        Scene scene = new Scene(canvas, 440, 440);
         Scene scene = new Scene(viewer, 440, 440);
         stage.setTitle("George's Saga!");
         stage.setScene(scene);
