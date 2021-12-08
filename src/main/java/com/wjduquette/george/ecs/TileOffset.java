d14fc3adcf6f6ff5351499abd0f5c5d014543dbd
Will Duquette <will@wjduquette.com>
2021-12-07 18:03:54 -0800

    Added TileOffset.ZERO.

--- src/main/java/com/wjduquette/george/ecs/TileOffset.java
+++ src/main/java/com/wjduquette/george/ecs/TileOffset.java
@@ -6,4 +6,6 @@ import javafx.geometry.Point2D;
  * Where the entity's Tile should be drawn, relative to its nominal position.
  * This is used by animations.
  */
-public record TileOffset(double rowOffset, double colOffset) { }
+public record TileOffset(double rowOffset, double colOffset) {
+    public static final TileOffset ZERO = new TileOffset(0.0, 0.0);
+}
