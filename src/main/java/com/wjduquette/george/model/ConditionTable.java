6576996607385ac35b7777fb71e04d8605e45715
Will Duquette <will@wjduquette.com>
2021-12-26 11:31:00 -0800

    Added preliminary condition table.

--- src/main/java/com/wjduquette/george/model/ConditionTable.java
+++ src/main/java/com/wjduquette/george/model/ConditionTable.java
@@ -1,15 +1,13 @@
 package com.wjduquette.george.model;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class ConditionTable {
     //-------------------------------------------------------------------------
     // Instance Variables
 
     // The table from variable name to variable
-    private final Map<String, ConditionVariable<?>> table =
+    private final Map<String, ConditionVariable> table =
         new HashMap<>();
 
     //-------------------------------------------------------------------------
@@ -26,48 +24,48 @@ public class ConditionTable {
      * Puts a condition variable into the table.
      * @param var The variable
      */
-    public void put(ConditionVariable<?> var) {
+    public void define(ConditionVariable var) {
         table.put(var.name(), var);
     }
 
     /**
      * Gets the named variable.
      * @param name The variable name
-     * @param <T> The variable's enumeration
      * @return The variable, or null if unknown
      */
-    public <T> T get(String name) {
-        return (T)table.get(name);
+    public ConditionVariable get(String name) {
+        return table.get(name);
     }
 
     /**
      * Finds the named variable.
      * @param name The variable name
-     * @param <T> The variable's enumeration
      * @return The variable, or empty if unknown
      */
-    public <T> Optional<T> find(String name) {
-        return Optional.ofNullable((T)table.get(name));
+    public Optional<ConditionVariable> find(String name) {
+        return Optional.ofNullable(table.get(name));
     }
 
     /**
      * Gets the current value of the variable, which must exist.
      * @param name The named variable
-     * @param <T> The variable's enumeration
      * @return The value
      */
-    public <T> T getValue(String name) {
-        return (T)table.get(name).get();
+    public String getValue(String name) {
+        return table.get(name).get();
     }
 
     /**
-     * Gets the current value of the variable, which must exist.
+     * Sets the current value of the variable, which must exist.
      * @param name The named variable
-     * @param <T> The variable's enumeration
      * @param value The new value
      */
-    public <T extends Enum<T>> void setValue(String name, T value) {
-        ConditionVariable<T> var = get(name);
-        var.set(value);
+    public void setValue(String name, String value) {
+        get(name).set(value);
     }
+
+    public Set<String> getNames() {
+        return table.keySet();
+    }
+
 }
--- src/main/java/com/wjduquette/george/model/ConditionVariable.java
+++ src/main/java/com/wjduquette/george/model/ConditionVariable.java
@@ -1,39 +1,53 @@
 package com.wjduquette.george.model;
 
-/**
- * A condition variable represent the state of some thing in the game:
- * the state of a quest, whether the player has ever entered a certain
- * region, etc.
- */
-public class ConditionVariable<T extends Enum<T>> {
+import java.util.Collections;
+import java.util.List;
+
+public final class ConditionVariable {
     //-------------------------------------------------------------------------
     // Instance Variables
 
+    // The name
     private final String name;
-    private T value;
+
+    // The valid values
+    private final List<String> values;
+
+    // The current value
+    private String value;
 
     //-------------------------------------------------------------------------
     // Constructor
 
     /**
-     * Creates a variable for the given enum with the name and initial value.
-     * @param name The variable name
-     * @param initialValue The variable's initial value
+     * Creates a new variable with the given name, range of values, and
+     * initial value.
+     * @param name The name
+     * @param values The values
+     * @param value The initial value
      */
-    public ConditionVariable(
-        String name,
-        T initialValue)
-    {
+    public ConditionVariable(String name, List<String> values, String value) {
         this.name = name;
-        this.value = initialValue;
+        this.values = values;
+        this.value = requireValid(value);
+    }
+
+    /**
+     * Creates a new variable with the given name and range of values.
+     * The initial value will be the first in the list.
+     * @param name The name
+     * @param values The values
+     */
+    public ConditionVariable(String name, List<String> values) {
+        this(name, values, values.get(0));
     }
 
     //-------------------------------------------------------------------------
     // Public API
 
     /**
-     * Gets the variable's name.
-     * @return The name`
+     * Returns the name.
+     * @return The name
      */
     public String name() {
         return name;
@@ -41,17 +55,35 @@ public class ConditionVariable<T extends Enum<T>> {
 
     /**
      * Gets the value.
-     * @return The value
+     * @return the value
      */
-    public T get() {
+    public String get() {
         return value;
     }
 
     /**
-     * Sets the value.
-     * @param newValue The value
+     * Sets the value, which must be valid.
+     * @param value The new value
      */
-    public void set(T newValue) {
-        this.value = newValue;
+    public void set(String value) {
+        this.value = requireValid(value);
+    }
+
+    /**
+     * Gets the list of valid values.
+     * @return The list
+     */
+    public List<String> values() {
+        return Collections.unmodifiableList(values);
+    }
+
+    // Returns the value, if valid, and throws an exception otherwise.
+    private String requireValid(String value) {
+        if (!values.contains(value)) {
+            throw new IllegalArgumentException("Invalid value: " + value +
+                ", should be in " + values);
+        } else {
+            return value;
+        }
     }
 }
