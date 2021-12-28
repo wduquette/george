d1e03588906dc21c1d7707b02b1d921948c0ea68
Will Duquette <will@wjduquette.com>
2021-12-28 09:50:46 -0800

    Added a dialog for Joe Pro the tutor.

--- src/main/java/com/wjduquette/george/regions/OverworldRegion.java
+++ src/main/java/com/wjduquette/george/regions/OverworldRegion.java
@@ -1,10 +1,14 @@
 package com.wjduquette.george.regions;
 
 import com.wjduquette.george.App;
-import com.wjduquette.george.model.DataDrivenRegion;
-import com.wjduquette.george.model.Step;
-import com.wjduquette.george.model.Trigger;
+import com.wjduquette.george.ecs.Entity;
+import com.wjduquette.george.model.*;
 import com.wjduquette.george.tmx.TiledMapReader;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
 
 public class OverworldRegion extends DataDrivenRegion {
     //-------------------------------------------------------------------------
@@ -31,6 +35,90 @@ public class OverworldRegion extends DataDrivenRegion {
 
                 return true;
             default: return false;
+        }
+    }
+
+    @Override
+    public final Optional<NPCDialog> findDialog(long id) {
+        var entity = get(id);
+        if (entity.mannikin() != null &&
+            entity.mannikin().key().equals("overworld.tutor"))
+        {
+            return Optional.of(new TutorDialog(entity));
+        } else {
+            return super.findDialog(id);
+        }
+    }
+
+    //-------------------------------------------------------------------------
+    // NPCs
+
+    private class TutorDialog implements NPCDialog {
+        enum State {
+            START,
+            LEFTCLICK,
+            INTERACT,
+            FINAL,
+            END
+        }
+
+        //---------------------------------------------------------------------
+        // Instance Variables
+
+        private final Entity npc;
+        private final String key;
+        private State state = State.START;
+
+        //---------------------------------------------------------------------
+        // Constructor
+
+        TutorDialog(Entity npc) {
+            this.npc = npc;
+            this.key = npc.mannikin().key();
+        }
+
+        @Override
+        public String foregroundSprite() {
+            return npc.sprite().name();
+        }
+
+        @Override
+        public String backgroundSprite() {
+            return getTerrain(npc.cell()).name();
+        }
+
+        @Override
+        public boolean isComplete() {
+            return state == State.END;
+        }
+
+        @Override
+        public String getDisplayText() {
+            return getInfo(key, state.toString().toLowerCase());
+        }
+
+        @Override
+        public List<Response> getResponses() {
+            List<Response> result = new ArrayList<>();
+
+            switch (state) {
+                case START ->
+                    result.add(new Response(State.LEFTCLICK, "Left-click?"));
+                case LEFTCLICK ->
+                    result.add(new Response(State.INTERACT, "Interact?"));
+                case INTERACT ->
+                    result.add(new Response(State.FINAL, "Anything Else?"));
+                default -> {}
+            }
+
+            result.add(new Response(State.END, "Goodbye."));
+
+            return result;
+        }
+
+        @Override
+        public void respond(Response response) {
+            state = (State)response.tag();
         }
     }
 }
