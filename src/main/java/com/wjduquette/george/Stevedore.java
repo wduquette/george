7efcbfb08a2c07fb5791484450e9d5129983efc9
Will Duquette <will@wjduquette.com>
2022-01-01 11:08:27 -0800

    Added preliminary `Stevedore.getSlotBox`

--- src/main/java/com/wjduquette/george/Stevedore.java
+++ src/main/java/com/wjduquette/george/Stevedore.java
@@ -2,8 +2,13 @@ package com.wjduquette.george;
 
 import com.wjduquette.george.ecs.Entity;
 import com.wjduquette.george.ecs.ItemStack;
+import com.wjduquette.george.model.ItemSlot;
 import com.wjduquette.george.model.Region;
+import com.wjduquette.george.widgets.Action;
+import com.wjduquette.george.widgets.SlotBox;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -11,6 +16,69 @@ import java.util.Optional;
  */
 public class Stevedore {
     private Stevedore() {} // Not instantiable
+
+    /**
+     * Gets the SlotBox for the given item slot.
+     * TODO: This doesn't work.
+     * - The panel from which the actions are called needs to repaint after
+     *   each action.
+     * - The SlotBox doesn't need to have the actions; we can produce the
+     *   actions just for the selected slot on repaint.
+     * @param region The region
+     * @param owner The owner
+     * @param itemSlot The slot
+     * @return The SlotBox
+     */
+    public static SlotBox getSlotBox(
+        Region region,
+        Entity owner,
+        ItemSlot itemSlot)
+    {
+        return switch (itemSlot) {
+            case ItemSlot.Inventory slot -> invSlotBox(region, owner, slot);
+            case ItemSlot.Party slot -> partySlotBox(region, owner, slot);
+            case ItemSlot.Equipment slot -> equipSlotBox(region, owner, slot);
+        };
+    }
+
+    private static SlotBox invSlotBox(
+        Region region,
+        Entity owner,
+        ItemSlot.Inventory slot
+    ) {
+        var count = owner.inventory().count(slot.index());
+        var item = owner.inventory().peek(slot.index());
+        var box = new SlotBox(slot, count, item);
+
+        if (count > 0) {
+            if (item.item().type().isUsable()) {
+                box.actions().add(new Action("Use",
+                    () -> useItem(region, owner, slot.index())));
+            }
+            box.actions().add(new Action("Drop",
+                () -> dropItem(region, owner, slot.index())));
+        }
+
+        return box;
+    }
+
+    private static SlotBox partySlotBox(
+        Region region,
+        Entity owner,
+        ItemSlot.Party slot)
+    {
+        // TODO
+        return null;
+    }
+
+    private static SlotBox equipSlotBox(
+        Region region,
+        Entity owner,
+        ItemSlot.Equipment slot)
+    {
+        // TODO
+        return null;
+    }
 
     /**
      * Attempts to drop one of the owner's items item on the ground.
@@ -37,6 +105,18 @@ public class Stevedore {
         stack.inventory().add(item);
 
         region.log("Dropped " + item.label().text());
+        return true;
+    }
+
+    public static boolean useItem(Region region, Entity owner, int index) {
+        // FIRST, get the item to use
+        var item = owner.inventory().take(index).orElseThrow();
+
+        region.log("Used: " + item.label().text());
+
+        // TODO: switch on the item type to make stuff happen.
+        // TODO: Return false if the item can't be used in this context.
+
         return true;
     }
 
