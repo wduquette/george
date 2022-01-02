e45e1620e49f638f9386328e37d469d540eca482
Will Duquette <will@wjduquette.com>
2022-01-02 11:55:45 -0800

    Simplified Equip.

--- src/main/java/com/wjduquette/george/model/Equip.java
+++ src/main/java/com/wjduquette/george/model/Equip.java
@@ -5,25 +5,20 @@ package com.wjduquette.george.model;
  */
 public enum Equip {
     /** Armor worn on the PC's body. */
-    BODY("equip.armor"),
+    BODY,
 
     /** Armor worn on the PC's head. */
-    HEAD("equip.helmet"),
+    HEAD,
 
     /** Armor worn on the PC's feet. */
-    FEET("equip.footwear"),
+    FEET,
 
     /** Hand-to-hand weapon wielded by the PC. */
-    HAND("equip.weapon"),
+    HAND,
 
     /** Ranged-weapon wielded by the PC. */
-    RANGED("equip.box"),
+    RANGED,
 
     /** Shield wielded by the PC. */
-    SHIELD("equip.shield");
-
-    // The slot's info key
-    private final String key;
-
-    Equip(String key) { this.key = key; }
+    SHIELD,
 }
