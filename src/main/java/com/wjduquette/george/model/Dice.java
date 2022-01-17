541d0940f8920a889d43272a482d6f9a52b994ab
Will Duquette <will@wjduquette.com>
2022-01-17 09:56:44 -0800

    Added dice spec parser.

--- src/main/java/com/wjduquette/george/model/Dice.java
+++ src/main/java/com/wjduquette/george/model/Dice.java
@@ -2,6 +2,8 @@ package com.wjduquette.george.model;
 
 import com.wjduquette.george.App;
 
+import java.util.Scanner;
+
 /**
  * Dice to roll.
  * @param number The number of dice
@@ -11,14 +13,14 @@ import com.wjduquette.george.App;
 public record Dice(int number, int sides, int plus) {
     @Override public String toString() {
         var n = number > 0 ? Integer.toString(number) : "";
-        var p = plus != 0 ? "+" + Integer.toString(plus) : "";
+        var p = plus != 0 ? "+" + plus : "";
 
         return n + "D" + sides + p;
     }
 
     /**
      * Rolls the dice and returns the result.
-     * @return
+     * @return The quantity rolled.
      */
     public int roll() {
         int result = plus;
@@ -28,5 +30,37 @@ public record Dice(int number, int sides, int plus) {
         }
 
         return result;
+    }
+
+    /**
+     * Converts a string of the form [{number}]D{sides}[+{plus}] into a
+     * Dice value.
+     * @param spec The spec
+     * @return The Dice
+     * @throws IllegalArgumentException if the spec can't be parsed.
+     */
+    public static Dice valueOf(String spec) {
+        try (Scanner scanner = new Scanner(spec.trim().toLowerCase())) {
+            scanner.useDelimiter("");
+            var num = scanner.hasNextInt() ? scanner.nextInt() : 1;
+            scanner.skip("d");
+            var sides = scanner.nextInt();
+
+            int plus = 0;
+            if (scanner.hasNext("\\+")) {
+                scanner.skip("\\+");
+                plus = scanner.nextInt();
+            }
+
+            if (scanner.hasNext()) {
+                throw new IllegalArgumentException(
+                    "Unexpected token at end of spec: \"" + scanner.next() + "\"");
+            }
+
+            return new Dice(num, sides, plus);
+        } catch (RuntimeException ex) {
+            throw new IllegalArgumentException(
+                "Invalid Dice spec: \"" + spec + "\", " + ex.getMessage(), ex);
+        }
     }
 }
